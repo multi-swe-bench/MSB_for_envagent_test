6b34c7a4d48e7682453e2dd7b1fa1d1fb592ce0c
aoyanli <aoyanli.722@bytedance.com>
2025-06-26 12:22:24 +0000

    Update files by envagent

--- multi_swe_bench/harness/repos/typescript/facebook/__init__.py
+++ multi_swe_bench/harness/repos/typescript/facebook/__init__.py
@@ -1,2 +1,3 @@
 from multi_swe_bench.harness.repos.typescript.remix_run.react_router import *
 from multi_swe_bench.harness.repos.typescript.facebook.lexical import *
+from multi_swe_bench.harness.repos.typescript.facebook.docusaurus import *
--- multi_swe_bench/harness/repos/typescript/remix_run/__init__.py
+++ multi_swe_bench/harness/repos/typescript/remix_run/__init__.py
@@ -1,1 +1,2 @@
 from multi_swe_bench.harness.repos.typescript.facebook.docusaurus import *
+from multi_swe_bench.harness.repos.typescript.remix_run.react_router import *
